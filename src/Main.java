558771b54b7d283bda7ae075878c75985cb41013
Yue Wang <yue.wang.data2018@gmail.com>
2018-05-19 11:54:10 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello World");
+        System.out.println("I am using git");
+    }
+}
